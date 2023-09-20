8d306e62e389d380345aa56c05808a015e4a8d21
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-09-20 14:43:30 -0700

    AirBNB Clone Version 2

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,4 @@
+-- prepares a MySQL server for the project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
