681f971441d7c9ddc7fb2e0f3a368ebb63cae0ef
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-10-05 18:54:32 -0700

    Pycodestyle

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,15 +2,20 @@
 """ Web Server """
 import time
 from fabric.api import local
- 
+
 file_path = "versions/web_static_{}.tgz".format(time.strftime("%Y%m%d%H%M%S"))
 
-def do_pack():
-        """ generates a .tgz archive from the contents of the web_static folder  """
-        local("mkdir -p versions")
-        arch = local("tar -cvzf {} web_static/".format(file_path))
 
-        if arch.succeeded:
-                return file_path
-        else:
-                return None
+def do_pack():
+    """
+    generates a .tgz archive from the
+    contents of the web_static folder
+    """
+
+    local("mkdir -p versions")
+    arch = local("tar -cvzf {} web_static/".format(file_path))
+
+    if arch.succeeded:
+        return file_path
+    else:
+        return None
