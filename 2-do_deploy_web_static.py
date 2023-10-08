71d42c1d732923e4469005455f944300c6c0074f
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-10-08 10:42:31 -0700

    web deployment

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ Web Server """
 import os
-from fabric.api import run, put, env
+import time
+from fabric.api import local, run, put, env
 
 env.hosts = ['54.157.184.108', '52.3.248.81']
-env.user = 'unbuntu'
+env.user = "ubuntu"
 
 def do_deploy(archive_path):
     """ distributes an archive to web servers """
@@ -13,22 +14,23 @@ def do_deploy(archive_path):
 
     try:
         arch_filename = os.path.basename(archive_path)
-        new_path = '/data/web_static/releases/{}'.format(
+        new_path = '/data/web_static/releases/{}/'.format(
                     arch_filename[:-4])
         put(archive_path, "/tmp/")
 
-        run("mkdir -p {}/".format(new_path))
-        run("tar -xzf /tmp/{} -C {}/".format(arch_filename,
+        run("mkdir -p {}".format(new_path))
+        run("tar -xzf /tmp/{} -C {}".format(arch_filename,
                                             new_path))
 
         run("rm /tmp/{}".format(arch_filename))
 
-        run("mv {}/web_static/* {}/".format(new_path, new_path))
-        run("rm -rf {}/web_static".format(new_path))
+        run("mv {}web_static/* {}/".format(new_path, new_path))
+        run("rm -rf {}web_static".format(new_path))
 
         run("rm -rf /data/web_static/current")
-        run("ln -s {}/ /data/web_static/current".format(new_path))
+        run("ln -s {} /data/web_static/current".format(new_path))
         print("New version deployed!")
         return True
     except Exception:
         return False
+
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -32,7 +32,7 @@ def do_deploy(archive_path):
 
     try:
         arch_filename = os.path.basename(archive_path)
-        new_path = '/data/web_static/releases/{}'.format(
+        new_path = '/data/web_static/releases/{}/'.format(
                     arch_filename[:-4])
         put(archive_path, "/tmp/")
 
@@ -42,8 +42,8 @@ def do_deploy(archive_path):
 
         run("rm /tmp/{}".format(arch_filename))
 
-        run("mv {}/web_static/* {}/".format(new_path, new_path))
-        run("rm -rf {}/web_static".format(new_path))
+        run("mv {}web_static/* {}/".format(new_path, new_path))
+        run("rm -rf {}web_static".format(new_path))
 
         run("rm -rf /data/web_static/current")
         run("ln -s {} /data/web_static/current".format(new_path))
