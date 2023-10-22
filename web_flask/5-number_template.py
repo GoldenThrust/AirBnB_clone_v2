b76d63568d4b0150432bc2c424309e20dd3cbaf0
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-10-22 10:46:04 -0700

    Testing

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -3,41 +3,40 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello():
     """ display Hello HBNB """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ display HBNB """
     return 'HBNB'
 
 
-@app.route('/c/<text>')
+@app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
     """ display C with <text> value """
     return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route('/python')
-@app.route('/python/<text>')
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def python_is_cool(text='is cool'):
     """ display python with <text> value """
     return 'Python {}'.format(text.replace('_', ' '))
 
 
-@app.route('/number/<int:n>')
+@app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """ display <n> is a number only if <n> is an integer """
     return '{:d} is a number'.format(n)
 
 
-@app.route('/number_template/<int:n>')
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """ display HTML page only if <n> is an integer """
     return render_template("5-number.html", n=n)
