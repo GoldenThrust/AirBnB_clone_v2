ee584c56d38569785647ee0ff27905feee270be4
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-10-22 09:58:02 -0700

    Testing

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,12 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def hello():
     """ print Hello HBNB """
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,15 +3,18 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def hello():
     """ display Hello HBNB """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb')
 def hbnb():
     """ display HBNB """
     return 'HBNB!'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
