fb36e1e5eb6a9c7185371ea1a8f4318862d36f03
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-10-22 10:22:01 -0700

    Testing

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,15 +1,17 @@
+#!/usr/bin/python3
 from flask import Flask
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello():
     """ display Hello HBNB """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
     """ display HBNB """
     return 'HBNB'
